4fd72259f038efa3bb60f49ac78d3f618d93efaf
ajay <ajay_886@hotmail.com>
2016-01-30 01:59:59 +0530

    Convert stub to return observable list

--- src/main/java/com/spike/PatientService/controller/PatientController.java
+++ src/main/java/com/spike/PatientService/controller/PatientController.java
@@ -5,7 +5,10 @@ import com.spike.PatientService.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+import rx.Observable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,8 +18,20 @@ public class PatientController {
     PatientService patientService;
 
     @RequestMapping("/patient")
-    public List<Patient> getAllPatients(){
-        return patientService.getAllPatients();
+    public DeferredResult<List<Patient>> getAllPatients(){
+        Observable<Patient> allPatients = patientService.getAllPatients();
+        DeferredResult<List<Patient>> patientDeferredResult = getDeferredResult(allPatients);
+        return patientDeferredResult;
+    }
+
+    private DeferredResult<List<Patient>> getDeferredResult(Observable<Patient> allPatients) {
+        DeferredResult<List<Patient>> patientDeferredResult = new DeferredResult<>(9000L);
+        List<Patient> patients = new ArrayList<>();
+        allPatients.subscribe(patient -> patients.add(patient),
+                error -> patientDeferredResult.setErrorResult(error),
+                () ->  patientDeferredResult.setResult(patients));
+
+        return patientDeferredResult;
     }
 
 
--- src/main/java/com/spike/PatientService/service/PatientService.java
+++ src/main/java/com/spike/PatientService/service/PatientService.java
@@ -5,6 +5,7 @@ import com.spike.PatientService.model.Order;
 import com.spike.PatientService.model.Patient;
 import com.spike.PatientService.model.Refill;
 import org.springframework.stereotype.Service;
+import rx.Observable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,14 +13,14 @@ import java.util.List;
 @Service
 public class PatientService {
 
-    public List<Patient> getAllPatients(){
+    public Observable<Patient> getAllPatients(){
         List<Refill> refillList1 = Arrays.asList(new Refill("p1"), new Refill("p2"));
         Address address1 = new Address("add1", "add2", "city", "zip1", "state1");
         Order order1 = new Order(1, address1, refillList1);
         Patient patient1 = new Patient(1, "fname1", "lname1", order1);
         Order order2 = new Order(2, address1, refillList1);
         Patient patient2 = new Patient(2, "fname2", "lname2", order2);
-        return Arrays.asList(patient1, patient2);
+        return rx.Observable.just(patient1, patient2);
     }
 
 }

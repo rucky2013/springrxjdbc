1c389ca76927b244b222bbd298c0fa6a3e97fc72
ajay <ajay_886@hotmail.com>
2016-01-29 23:22:15 +0530

    Initial skeleton

---
+++ src/main/java/com/spike/PatientService/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.spike.PatientService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}

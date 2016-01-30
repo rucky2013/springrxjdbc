14fc616c835e4210d98904fecdc503e8ed927bb3
ajay <ajay_886@hotmail.com>
2016-01-30 14:05:09 +0530

    Read db connection details from properties file

--- src/main/java/com/spike/PatientService/repository/PatientRepository.java
+++ src/main/java/com/spike/PatientService/repository/PatientRepository.java
@@ -2,14 +2,24 @@ package com.spike.PatientService.repository;
 
 import com.github.davidmoten.rx.jdbc.Database;
 import com.spike.PatientService.model.Patient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import rx.Observable;
 
 @Repository
 public class PatientRepository {
 
+    @Value("${db.connection}")
+    private String dbConnectionString;
+
+    @Value("${db.username}")
+    private String dbUserName;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
     public Observable<Patient> getAllPatients(){
-        Database db = Database.from("jdbc:postgresql://127.0.0.1:5432/dev", "dev", "dev");
+        Database db = Database.from(dbConnectionString, dbUserName, dbPassword);
         return db.select("select id, first_name, last_name from PATIENT order by last_name")
                 .autoMap(Patient.class);
     }
--- src/main/java/com/spike/PatientService/service/PatientService.java
+++ src/main/java/com/spike/PatientService/service/PatientService.java
@@ -1,16 +1,10 @@
 package com.spike.PatientService.service;
 
-import com.spike.PatientService.contract.Address;
-import com.spike.PatientService.contract.Order;
 import com.spike.PatientService.contract.Patient;
-import com.spike.PatientService.contract.Refill;
 import com.spike.PatientService.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rx.Observable;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class PatientService {

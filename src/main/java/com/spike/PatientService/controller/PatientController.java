b1928ca7e585dea4c08743f8268f84785a6cebbb
ajay <ajay_886@hotmail.com>
2016-01-31 22:09:36 +0530

    Added Address Service

--- src/main/java/com/spike/PatientService/controller/PatientController.java
+++ src/main/java/com/spike/PatientService/controller/PatientController.java
@@ -1,10 +1,15 @@
 package com.spike.PatientService.controller;
 
+import com.spike.PatientService.contract.Address;
 import com.spike.PatientService.contract.Patient;
+import com.spike.PatientService.service.AddressService;
 import com.spike.PatientService.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import rx.Observable;
@@ -18,6 +23,12 @@ public class PatientController {
     @Autowired
     PatientService patientService;
 
+    @Autowired
+    AddressService addressService;
+
+    @Value("${response.timeout}")
+    private long timeOut;
+
     @RequestMapping("/patient")
     public DeferredResult<List<Patient>> getAllPatients(){
         Observable<Patient> allPatients = patientService.getAllPatients();
@@ -25,8 +36,24 @@ public class PatientController {
         return patientDeferredResult;
     }
 
+    @RequestMapping("/patient/{id}/addresses")
+    public DeferredResult<List<Address>> getAddressesByPatient(@PathVariable("id") int patientId){
+        Observable<Address> addressesForPatient = addressService.getAddressesForPatient(patientId);
+        DeferredResult<List<Address>> addressesDeferredResult = getAddressesDeferredResult(addressesForPatient);
+        return addressesDeferredResult;
+    }
+
+    private DeferredResult<List<Address>> getAddressesDeferredResult(Observable<Address> addressesForPatient) {
+        DeferredResult<List<Address>> deferredResult = new DeferredResult<>(timeOut);
+        List<Address> addresses = new ArrayList<>();
+        addressesForPatient.subscribe(add -> addresses.add(add),
+                error -> deferredResult.setErrorResult(error),
+                () -> deferredResult.setResult(addresses));
+        return deferredResult;
+    }
+
     private DeferredResult<List<Patient>> getDeferredResult(Observable<Patient> allPatients) {
-        DeferredResult<List<Patient>> patientDeferredResult = new DeferredResult<>(9000L);
+        DeferredResult<List<Patient>> patientDeferredResult = new DeferredResult<>(timeOut);
         List<Patient> patients = new ArrayList<>();
         allPatients.subscribe(patient -> patients.add(patient),
                 error -> patientDeferredResult.setErrorResult(error),
--- src/main/java/com/spike/PatientService/service/AddressService.java
+++ src/main/java/com/spike/PatientService/service/AddressService.java
@@ -1,11 +1,14 @@
 package com.spike.PatientService.service;
 
 import com.spike.PatientService.contract.Address;
+import com.spike.PatientService.repository.AddressesByPatientCommand;
 import com.spike.PatientService.repository.AddressesByPatientCommandFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import rx.Observable;
+
+import java.util.List;
 
 @Service
 public class AddressService {
@@ -14,6 +17,19 @@ public class AddressService {
     private AddressesByPatientCommandFactory commandFactory;
 
     public Observable<Address> getAddressesForPatient(int patientId){
-        return null;
+        AddressesByPatientCommand command = commandFactory.createCommand(patientId);
+        List<com.spike.PatientService.model.Address> addresses = command.execute();
+        return Observable.from(addresses).map(add -> mapAddress(add));
+    }
+
+    private Address mapAddress(com.spike.PatientService.model.Address addressModel) {
+        Address address = new Address();
+        address.setAddress1(addressModel.address1());
+        address.setAddress2(addressModel.address2());
+        address.setCity(addressModel.city());
+        address.setZipCode(addressModel.zipCode());
+        address.setState(addressModel.state());
+        address.setType(addressModel.type());
+        return address;
     }
 }
